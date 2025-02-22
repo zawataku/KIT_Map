e8f88d365415b14c47e529f7afa6b8d89ed414b5
zawataku <c1254976@st.kanazawa-it.ac.jp>
2025-02-23 01:03:38 +0900

    feat: material_floating_search_bar_2 package

--- lib/widgets/campus_map_page.dart
+++ lib/widgets/campus_map_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'material_floating_search_bar.dart';
 
 class CampusMapPage extends StatelessWidget {
   const CampusMapPage({super.key});
@@ -8,36 +9,35 @@ class CampusMapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Campus Map（扇が丘キャンパス）",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            )),
-      ),
-      body: FlutterMap(
-        options: MapOptions(
-          initialCenter: const LatLng(36.530408, 136.627638),
-          initialZoom: 17,
-          minZoom: 17.0,
-          maxZoom: 20.0,
-          cameraConstraint: CameraConstraint.contain(
-            bounds: LatLngBounds(
-              const LatLng(36.533430, 136.633422),
-              const LatLng(36.526298, 136.624726),
-            ),
-          ),
-          interactionOptions: const InteractionOptions(
-              flags: InteractiveFlag.pinchZoom | InteractiveFlag.drag),
-        ),
+      body: Stack(
         children: [
-          TileLayer(
-            tileProvider: AssetTileProvider(),
-            maxZoom: 20,
-            urlTemplate: "assets/tiles/{z}/{x}/{y}.png",
-            tileSize: 256,
-            minNativeZoom: 17,
-            maxNativeZoom: 20,
+          FlutterMap(
+            options: MapOptions(
+              initialCenter: const LatLng(36.530408, 136.627638),
+              initialZoom: 17,
+              minZoom: 17.0,
+              maxZoom: 20.0,
+              cameraConstraint: CameraConstraint.contain(
+                bounds: LatLngBounds(
+                  const LatLng(36.533430, 136.633422),
+                  const LatLng(36.526298, 136.624726),
+                ),
+              ),
+              interactionOptions: const InteractionOptions(
+                  flags: InteractiveFlag.pinchZoom | InteractiveFlag.drag),
+            ),
+            children: [
+              TileLayer(
+                tileProvider: AssetTileProvider(),
+                maxZoom: 20,
+                urlTemplate: "assets/tiles/{z}/{x}/{y}.png",
+                tileSize: 256,
+                minNativeZoom: 17,
+                maxNativeZoom: 20,
+              ),
+            ],
           ),
+          const FloatingSearchAppBar(),
         ],
       ),
     );
---
+++ lib/widgets/material_floating_search_bar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
+
+class FloatingSearchAppBar extends StatelessWidget {
+  const FloatingSearchAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return buildFloatingSearchBar();
+  }
+
+  Widget buildFloatingSearchBar() {
+    return FloatingSearchBar(
+      hint: '号館を検索',
+      borderRadius: BorderRadius.circular(25),
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 500),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      openAxisAlignment: 0.0,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {
+        // Call your model, bloc, controller here.
+      },
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: color);
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
